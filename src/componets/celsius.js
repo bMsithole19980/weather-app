066cb4f9a7be587a4c80b40834ed2f6244945c8a
bMsithole19980 <bmsithole1998@gmail.com>
2023-07-20 15:59:15 +0200

    first commit

--- src/componets/celsius.js
+++ src/componets/celsius.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 function Celsius() {
     const [city, setCity] = useState('');
     const [weatherInfo, setWeatherInfo] = useState(null);
-    const [error, setError] = useState(null)
+    const [error, setError] = useState(null);
+    const [iconUrl, setIconUrl]=useState(null);
 
     const apiKey = 'f0017941ecbaa103e461acb8fdca5c43';
 
@@ -37,6 +38,7 @@ function Celsius() {
 
     }
 
+    
     return (
         <div className='container'>
             <div className='heading'>
@@ -76,12 +78,14 @@ function Celsius() {
 
                 </div>
                 <div className='pages'>
-                    <button className='far'><Link to='/home' style={{color:"orange" , textDecoration:"none"}}>Fahrenheit</Link> </button>
-                    <button className='cels'>Celsius</button>
+                    <button className='far'> Fahrenheit</button>
+                    <button className='cels'><Link to='/celsius' style={{color:"purple" , textDecoration:"none"}}>Celsius</Link></button>
                 </div>
             </div>
 
             <div className='weather-content'>
+            {iconUrl && <img className='weather-cond' src={iconUrl} alt='condition- icon'/>}
+
                 <h3 className='rain'>Rain</h3>
                 <svg
                  xmlns="http://www.w3.org/2000/svg" 
@@ -94,7 +98,7 @@ function Celsius() {
                  textRendering="geometricPrecision" 
                  viewBox="0 0 6.827 6.827" 
                  id="humidity"><path fill="#1e88e5" d="M3.442.885c.247.268.483.547.703.837.184.245.358.498.513.762.234.4.437.844.53 1.3.035.178.055.359.05.54a1.82 1.82 0 0 1-.07.464c-.078.27-.223.508-.43.7-.354.33-.846.485-1.325.485-.478 0-.97-.156-1.325-.485a1.527 1.527 0 0 1-.43-.7 1.82 1.82 0 0 1-.07-.464c-.004-.181.015-.362.051-.54.093-.456.296-.9.53-1.3.155-.264.329-.517.513-.762.22-.29.456-.569.703-.837l.029-.032.028.032z"></path><path fill="#fff" d="M3.138 3.557a.252.252 0 0 0-.196-.083c-.08 0-.145.025-.192.091a.4.4 0 0 0-.07.239c0 .09.014.197.076.267a.24.24 0 0 0 .188.084c.078 0 .14-.029.192-.086a.372.372 0 0 0 .08-.254c0-.09-.016-.19-.079-.258zM3.734 3.434 2.947 4.95h.148l.784-1.516zM3.873 4.229c-.08 0-.146.025-.194.09a.4.4 0 0 0-.07.24c0 .09.015.197.077.267a.24.24 0 0 0 .188.084.249.249 0 0 0 .193-.085.372.372 0 0 0 .08-.255c0-.09-.016-.19-.078-.257a.252.252 0 0 0-.196-.084z"></path><path fill="#1e88e5" d="M3.03 3.703a.11.11 0 0 0-.084-.037c-.035 0-.064.012-.084.04a.174.174 0 0 0-.03.104c0 .039.006.085.033.116a.104.104 0 0 0 .081.036c.034 0 .061-.012.084-.037a.161.161 0 0 0 .034-.11c0-.04-.006-.083-.034-.112zm.93.755a.11.11 0 0 0-.084-.036c-.035 0-.063.01-.084.04a.174.174 0 0 0-.03.103c0 .04.006.086.033.116a.104.104 0 0 0 .082.037c.033 0 .06-.013.083-.038a.161.161 0 0 0 .035-.11c0-.039-.007-.082-.035-.112z"></path><path fill="none" d="M0 0h6.827v6.827H0z"></path></svg>
-
+                 
                 {weatherInfo && (
                     <div>
                         <div className='display-weather'>
@@ -127,8 +131,8 @@ function Celsius() {
                 )}
 
             </div>
-
-            <div className='weather-box'>
+            <div className='weather-box-container'>
+              <div className='weather-box'>
                 <div className='wind-icon'>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -194,13 +198,9 @@ function Celsius() {
                 <h3 className='details'>{weatherInfo?.weather[0].main} %</h3>
 
             </div>
+            </div>
 
-
-
-
-
-
-
+            
 
 
 
